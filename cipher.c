425b3e6afef650f90401967c4ce649aa03ed4ff2
Shreyas <shreyaskowshik@gmail.com>
2021-10-05 10:05:19 +0530

    Remove redundant comments

--- cipher.c
+++ cipher.c
@@ -346,24 +346,6 @@ const uint8_t fietsel_sbox[256] = {
   0x00, 0x00, 0x0b, 0x0d, 0x0e, 0x0a, 0x05, 0x00, 0x08, 0x0b, 0x0b, 0x0c, 0x03, 0x03, 0x09, 0x01,
   0x07, 0x0b, 0x04, 0x0e, 0x0a, 0x07, 0x06, 0x06, 0x01, 0x09, 0x04, 0x03, 0x05, 0x01, 0x0c, 0x0d };
 
-// const uint8_t invfietsel_sbox[256] = {
-//   0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
-//   0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
-//   0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
-//   0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
-//   0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
-//   0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
-//   0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
-//   0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
-//   0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
-//   0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
-//   0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
-//   0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
-//   0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
-//   0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
-//   0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
-//   0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16 };
-
 const uint8_t fietsel_permute_table[8] = {
     3, 4, 6, 5, 1, 0, 7, 2
 };
@@ -463,8 +445,6 @@ uint8_t** keyScheduler(uint8_t key[16], int rounds) {
 
     for(i=0;i<rounds;i++) { keys[i]=(uint8_t *)malloc(sizeof(uint8_t)*16); }
 
-    // printf("Malloc done\n");
-
     for(round=0;round<rounds;round++) {
         // printf("%d\n", round);
 
@@ -527,75 +507,44 @@ void fietsel_f(uint8_t *in, uint8_t *key) {
     
     uint8_t* expanded_in=Expansion(in);
 
-    // printf("Expanded Key 0 : "); print_as_hex_arr(expanded_in); printf("\n");
-
     int i;
     for(i=0;i<16;i++) expanded_in[i]=expanded_in[i]^key[i];
-    // printf("Expanded Key 1 : "); print_as_hex_arr(expanded_in); printf("\n");
 
     // apply sbox and reduce down to 8 bytes
     for(i=0;i<16;i++) expanded_in[i]=fietsel_sbox[expanded_in[i]];
-    // printf("Expanded Key 2 : "); print_as_hex_arr(expanded_in); printf("\n");
 
     for(i=0;i<16;i+=2) expanded_in[i]=(expanded_in[i]<<4)^(expanded_in[i+1]&15);
-    // printf("Expanded Key 3 : "); print_as_hex_arr(expanded_in); printf("\n");
 
     for(i=0;i<8;i++) in[i]=expanded_in[2*i];
 
-    // printf("After expanded key 3 in : "); print_as_hex_arr64(in); printf("\n");
-
     // apply permutation
     permute(in);
-    // printf("After permutation in : "); print_as_hex_arr64(in); printf("\n");
 }
 
 
 void encrypt_f(uint8_t in[16], uint8_t key[16], uint8_t out[16], int rounds) {
     uint8_t** keys = keyScheduler(key, rounds);
 
-    // printf("Printing key schedule : ");
-
     int round;
     int i;
-    // for(i=0;i<rounds;i++) {
-    //     print_as_hex_arr(keys[i]);
-    //     printf("\n---");
-    // }
 
     for(i=0;i<16;i++) out[i]=in[i]; // copy initial input to output
 
-    // printf("Input copying done\n");
-
     for(round=0;round<rounds;round++) {
-        // printf("%d\n", round);
-        // print_as_hex_arr(out);
-        // printf("\n---\n");
         // fietsel cipher
-
-
         // split input
         uint8_t L[8];
         uint8_t R[8];
         for(i=0;i<8;i++) L[i]=out[i];
         for(i=8;i<16;i++) R[i-8]=out[i];
-        // printf("L : "); print_as_hex_arr64(L); printf("\n");
-        // printf("R : "); print_as_hex_arr64(R); printf("\n");
 
         fietsel_f(R, keys[round]); // R is now updated to new values
-        // printf("After fietsel L : "); print_as_hex_arr64(L); printf("\n");
-        // printf("After fietsel R : "); print_as_hex_arr64(R); printf("\n");
 
         for(i=0;i<8;i++) out[i]=out[i+8]; // copy original R
         for(i=8;i<16;i++) {
             out[i]=L[i-8]^R[i-8];
-            // printf("%d %d %d %d\n", i, L[i-8], R[i-8], out[i]);
         }
-
-        // printf("Out at end : "); print_as_hex_arr(out); printf("\n");
-        // printf("\n---\n");
     }
-
-    // printf("Coming out\n");
 }
 
 void decrypt_f(uint8_t in[16], uint8_t key[16], uint8_t out[16], int rounds) {
@@ -604,68 +553,30 @@ void decrypt_f(uint8_t in[16], uint8_t key[16], uint8_t out[16], int rounds) {
     int i;
     for(i=0;i<16;i++) out[i]=in[i]; // copy initial input to output
 
-    // printf("Input copying done\n");
-
     for(round=rounds-1;round>=0;round--) {
-        // printf("%d\n", round);
-        // print_as_hex_arr(out);
         // fietsel cipher
-
-
         // split input
         uint8_t L[8];
         uint8_t R[8];
         for(i=0;i<8;i++) L[i]=out[i];
         for(i=8;i<16;i++) R[i-8]=out[i];
-        // printf("L : "); print_as_hex_arr64(L); printf("\n");
-        // printf("R : "); print_as_hex_arr64(R); printf("\n");
-
 
         fietsel_f(L, keys[round]); // L is now updated to new values
-        // printf("After fietsel L : "); print_as_hex_arr64(L); printf("\n");
-        // printf("After fietsel R : "); print_as_hex_arr64(R); printf("\n");
         for(i=8;i<16;i++) out[i]=out[i-8];
         for(i=0;i<8;i++) out[i]=L[i]^R[i];
-
-        // printf("Out at end : "); print_as_hex_arr(out); printf("\n");
-
-        // printf("\n---\n");
     }
-
-    // printf("Coming out\n");
 }
 /*************************************/
-
 
 /* Combined Encryption/Decryption */
 void encrypt(uint8_t in[16], uint8_t key[16], uint8_t out[16], int rounds) {
     uint8_t** s=state_from_block(in);
     uint32_t w[44];
     keySchedule(key, w);
-    // print keySchedule
-    // printf("Printing Key Schedule\n");
-    // for(int i=0;i<11;i++) {
-    //     printf("Round %d\n", i);
-    //     print_as_hex_word(w[4*i]);
-    //     printf(" ");
-    //     print_as_hex_word(w[4*i + 1]);
-    //     printf(" ");
-    //     print_as_hex_word(w[4*i + 2]);
-    //     printf(" ");
-    //     print_as_hex_word(w[4*i + 3]);
-    //     printf("\n-------\n");
-    // }
-
-    // printf("Key Scheduling done\n");
     int round=0;
 
     uint8_t** keyMat=roundKeyMat(w[round*4], w[round*4 + 1], w[round*4 + 2], w[round*4 + 3]);
     AddRoundKey(s, keyMat);
-
-    // printf("Round Key Added\n");
-    // printf("After add round key %d\n", round);
-    // print_as_hex_state(s);
-    // printf("\n");
 
     for(round=1;round<=5;round++) {
         // printf("Encryption : Round %d Start : ", round);
@@ -763,12 +674,6 @@ void decrypt(uint8_t in[16], uint8_t key[16], uint8_t out[16], int rounds) {
     int round=10;
 
     uint8_t** keyMat=roundKeyMat(w[round*4], w[round*4 + 1], w[round*4 + 2], w[round*4 + 3]);
-    
-
-    // printf("Round Key Added\n");
-    // printf("After add round key %d\n", round);
-    // print_as_hex_state(s);
-    // printf("\n");
 
     for(round=10;round>=6;round--) {
         // printf("Decryption : Round %d Start : ", round);
@@ -857,137 +762,6 @@ void decrypt(uint8_t in[16], uint8_t key[16], uint8_t out[16], int rounds) {
 
 
 int main() {
-    // do some checks
-    /*
-    uint8_t in[16]; // 128-bit input
-    uint8_t out[16];
-
-    uint8_t test[4][4] = {
-        {0x87, 0x09, 0x6a, 0xd5},
-        {0x6e, 0x09, 0x6a, 0xd5},
-        {0x46, 0x09, 0x6a, 0xd5},
-        {0xa6, 0x09, 0x6a, 0xd5}
-    };
-    */
-
-    /*
-    int i;
-    int j;
-    for(i=0;i<4;i++) {
-        for(j=0;j<4;j++) {
-            printf("%d ", test[i][j]);
-        }
-        printf("\n");
-    }
-
-    SubBytesCheckArrayChanged(test);
-
-    for(i=0;i<4;i++) {
-        for(j=0;j<4;j++) {
-            printf("%d ", test[i][j]);
-        }
-        printf("\n");
-    }
-    */
-    
-    
-    /*
-    int i;
-    int j;
-    for(i=0;i<4;i++) {
-        for(j=0;j<4;j++) {
-            printf("%d ", test[i][j]);
-        }
-        printf("\n");
-    }
-
-    // SubBytes(test);
-    // ShiftRows(test);
-    MixColumns(test);
-
-    printf("---\n");
-    for(i=0;i<4;i++) {
-        for(j=0;j<4;j++) {
-            printf("%d ", test[i][j]);
-        }
-        printf("\n");
-    }
-
-    // InvSubBytes(test);
-    // InvShiftRows(test);
-    InvMixColumns(test);
-
-    printf("---\n");
-    for(i=0;i<4;i++) {
-        for(j=0;j<4;j++) {
-            printf("%d ", test[i][j]);
-        }
-        printf("\n");
-    }
-    */
-    
-
-    
-    // uint8_t i=0x03;
-    // uint8_t j=0x6e;
-    // uint8_t res=Multiply(i, j);
-    // uint8_t t=(0x15)^(0xb2)^(0x46)^(0xa6);
-    // printf("%d %d %d %d\n", i, j, res, t);
-
-    // rotword, subword tests
-    /*
-    uint32_t test=0x01200330;
-    uint32_t a=RotWord(test);
-    uint32_t b=SubWord(test);
-    // printf("%d %d %d\n", test, a, b);
-    print_as_hex_word(test);
-    printf(" ");
-    print_as_hex_word(a);
-    printf(" ");
-    print_as_hex_word(b);
-    */
-
-
-    // end2end tests
-    
-    // uint8_t in[16] = {
-    //     0x6b,
-    //     0xc1,
-    //     0xbe,
-    //     0xe2,
-    //     0x2e,
-    //     0x40,
-    //     0x9f,
-    //     0x96,
-    //     0xe9,
-    //     0x3d,
-    //     0x7e,
-    //     0x11,
-    //     0x73,
-    //     0x93,
-    //     0x17,
-    //     0x2a
-    // };
-
-    // uint8_t key[16] = {
-    //     0x2b,
-    //     0x7e,
-    //     0x15,
-    //     0x16,
-    //     0x28,
-    //     0xae,
-    //     0xd2,
-    //     0xa6,
-    //     0xab,
-    //     0xf7,
-    //     0x15,
-    //     0x88,
-    //     0x09,
-    //     0xcf,
-    //     0x4f,
-    //     0x3c
-    // };
-
     uint8_t in[16] = {
         0x01,
         0x07,
@@ -1041,10 +815,6 @@ int main() {
         0x15,
         0x00
     };
-
-    /*
-    Expected Output : 66e94bd4ef8a2c3b884cfa59ca342b2e
-    */
 
     uint8_t out[16];
     int i,j;
